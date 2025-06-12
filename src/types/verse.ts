220bbac002e23ec78ff6c44edcf2f5852f24158c
BurhanxGodhra <127852642+BurhanxGodhra@users.noreply.github.com>
2025-06-12 18:34:36 +0500

    Updated verse.ts

--- src/types/verse.ts
+++ src/types/verse.ts
@@ -2,7 +2,7 @@ export interface OpeningVerse {
   id: string;
   text: string;
   author: string;
-  language: 'English' | 'Arabic' | 'Urdu';
+  language: 'English' | 'French' | 'Arabic' | 'Lisan al-Dawah' | 'Urdu';
   date: string;
   day: number; // Day of the festival (1-10)
 }
