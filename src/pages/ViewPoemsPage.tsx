6003f32be3798cc7f2943f4338843cb04b97f822
BurhanxGodhra <127852642+BurhanxGodhra@users.noreply.github.com>
2025-06-12 18:33:07 +0500

    Updated ViewPoemsPage.tsx

--- src/pages/ViewPoemsPage.tsx
+++ src/pages/ViewPoemsPage.tsx
@@ -114,6 +114,8 @@ const ViewPoemsPage: React.FC = () => {
                 <option value="English">English</option>
                 <option value="Arabic">Arabic</option>
                 <option value="Urdu">Urdu</option>
+                <option value="Lisan al-Dawah">Lisan al-Dawah</option>
+                      <option value="French">French</option>
               </select>
             </div>
             
